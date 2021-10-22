dd61bfa7e841633b9b840c7383205378e9eca9ba
kadowning1 <kadowning1@gmail.com>
2021-10-22 09:15:24 -0400

    demo push

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -37,7 +37,7 @@ export default function Footer(props) {
                         type="button"
                         className="btn btn-outline-secondary"
                         onClick={props.checkAll}>
-                        Check All
+                        Complete All
                         </button>
                     <button
                         type="button"
